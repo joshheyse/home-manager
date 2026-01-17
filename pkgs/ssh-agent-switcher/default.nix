8294091a9b6f63c2382dac823458396a0efe9069
Josh Heyse <josh@heyse.us>
2026-01-16 22:05:33 -0600

    update ssh-agent-switcher

--- pkgs/ssh-agent-switcher/default.nix
+++ pkgs/ssh-agent-switcher/default.nix
@@ -1,27 +1,34 @@
 {
   lib,
-  buildGoModule,
+  rustPlatform,
   fetchFromGitHub,
 }:
-buildGoModule {
-  pname = "ssh-agent-switcher";
-  version = "unstable-2025-10-02"; # Update as needed
+let
+  cargoLock = ./Cargo.lock;
+in
+  rustPlatform.buildRustPackage {
+    pname = "ssh-agent-switcher";
+    version = "unstable-2025-10-02";
 
-  src = fetchFromGitHub {
-    owner = "jmmv";
-    repo = "ssh-agent-switcher";
-    rev = "main";
-    sha256 = "sha256-QsWngp0z7yNOamItmlLJp1K+drdXuAtuIQXcSDkDiQs=";
-  };
+    src = fetchFromGitHub {
+      owner = "jmmv";
+      repo = "ssh-agent-switcher";
+      rev = "main";
+      sha256 = "sha256-QsWngp0z7yNOamItmlLJp1K+drdXuAtuIQXcSDkDiQs=";
+    };
 
-  vendorHash = null;
+    cargoLock = {
+      lockFile = cargoLock;
+    };
 
-  doCheck = false;
+    postPatch = ''
+      cp ${cargoLock} Cargo.lock
+    '';
 
-  meta = {
-    description = "A highly configurable, multi-protocol DNS forwarding proxy";
-    homepage = "https://github.com/jmmv/ssh-agent-switcher.git";
-    license = lib.licenses.bsd3;
-    mainProgram = "ssh-agent-switcher";
-  };
-}
+    meta = {
+      description = "Daemon to proxy SSH agent connections to any valid forwarded agent";
+      homepage = "https://github.com/jmmv/ssh-agent-switcher";
+      license = lib.licenses.bsd3;
+      mainProgram = "ssh-agent-switcher";
+    };
+  }
