b263d44297a717860d3419d0eb0e0f0dcf8e1f05
Josh Heyse <josh@heyse.us>
2026-03-05 19:31:24 -0600

    remove image.nvim pdf rendering

--- modules/programs/shell/neovim/config/lua/plugins/image.lua
+++ modules/programs/shell/neovim/config/lua/plugins/image.lua
@@ -8,4 +8,16 @@ return {
     window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
     tmux_show_only_in_original_window = true,
   },
+  config = function(_, opts)
+    require("image").setup(opts)
+    -- Prevent image.nvim from treating PDFs as images (avoids ImageMagick
+    -- conversion that conflicts with mupager's own PDF rendering).
+    local magic = require "image.utils.magic"
+    local orig_detect = magic.detect_format
+    magic.detect_format = function(path)
+      local fmt = orig_detect(path)
+      if fmt == "pdf" then return nil end
+      return fmt
+    end
+  end,
 }
